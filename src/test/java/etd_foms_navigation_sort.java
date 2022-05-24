17c964141ee87164a1fa75b699a75381f61560ce
dvens81 <dvens81@mail.ru>
2022-05-24 16:31:54 +0400

    etd_foms_navigation_sort
    Прокликивает последовательно все подсистемы меню навигации слева и проверяет, что реестры расположены в алфавитном порядке

--- src/test/java/etd_foms_navigation_sort.java
+++ src/test/java/etd_foms_navigation_sort.java
@@ -13,13 +13,14 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-// Прокликивает последовательно все пункты меню навигации слева, включая вложенные пункты.
-// Для каждой страницы проверяет наличие реестров (элемента с локатором ".MuiGrid-spacing-xs-2 .MuiGrid-root")
+// Прокликивает последовательно все подсистемы меню навигации слева и проверяет, что реестры расположены в алфавитном порядке
 
-public class etd_foms_navigation {
+public class etd_foms_navigation_sort {
 
     private EventFiringWebDriver driver;
     private WebDriverWait wait;
@@ -27,12 +28,12 @@ public class etd_foms_navigation {
     public static class MyListener extends AbstractWebDriverEventListener {
         @Override
         public void beforeFindBy(By by, WebElement element, WebDriver driver) {
-            System.out.println(by);
+            //System.out.println(by);
         }
 
         @Override
         public void afterFindBy(By by, WebElement element, WebDriver driver) {
-            System.out.println(by + " found");
+            //System.out.println(by + " found");
         }
     }
 
@@ -44,7 +45,7 @@ public class etd_foms_navigation {
         driver.register(new MyListener());
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         //driver.manage().timeouts().pageLoadTimeout(6, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(1));
 
     }
 
@@ -53,7 +54,7 @@ public class etd_foms_navigation {
     }
 
     @Test
-    public void myFirstTest() throws InterruptedException {
+    public void myFirstTest() {
         driver.get("https://d-customer-balancer-iam-proxy-01.foms.novalocal/etd-front/");
         driver.findElement(By.cssSelector("#details-button")).click();
         driver.findElement(By.cssSelector("#proceed-link")).click();
@@ -69,40 +70,38 @@ public class etd_foms_navigation {
 
         List<WebElement> menu = driver.findElements(By.cssSelector(".treeItem-level-0 h6"));
 
+        ArrayList<String> sub_menu_List = new ArrayList<>();
+
         for (int i = 0; i < menu.size(); i++) {
             menu = driver.findElements(By.cssSelector(".treeItem-level-0 h6"));
             menu.get(i).click();
-            Assert.assertTrue(isElementPresent(driver, By.cssSelector(".MuiGrid-spacing-xs-2 .MuiGrid-root")));
 
-            int sub_menu = driver.findElements(By.cssSelector(".MuiCollapse-wrapperInner .treeItem-level-1 .treeItem-node h6")).size();
+            List<WebElement> sub_menu = driver.findElements(By.cssSelector(".bookmark__text"));
 
-            if (sub_menu > 0) {
-                List<WebElement> menu_sub = driver.findElements(By.cssSelector(".MuiCollapse-wrapperInner .treeItem-level-1 .treeItem-node h6"));
+            for (int j = 0; j < sub_menu.size(); j++) {
+                String attr_sub_menu = sub_menu.get(j).getAttribute("textContent");
+                String attr_sub_menu_registr = attr_sub_menu.toLowerCase();
+                sub_menu_List.add(j, attr_sub_menu_registr);
 
-                for (int j = 0; j < menu_sub.size(); j++) {
-                    menu_sub.get(j).click();
-                    TimeUnit.MILLISECONDS.sleep(300);
-                    Assert.assertTrue(isElementPresent(driver, By.cssSelector(".MuiGrid-spacing-xs-2 .MuiGrid-root")));
-
-                    int sub_menu2 = driver.findElements(By.cssSelector("li ul .MuiCollapse-root h6")).size();
-
-                    if (sub_menu2 > 0) {
-                        List<WebElement> menu_sub2 = driver.findElements(By.cssSelector("li ul .MuiCollapse-root h6"));
-                        for (int k = 0; k < menu_sub2.size(); k++) {
-                            menu_sub2 = driver.findElements(By.cssSelector("li ul .MuiCollapse-root h6"));
-                            menu_sub2.get(k).click();
-                            Assert.assertTrue(isElementPresent(driver, By.cssSelector(".MuiGrid-spacing-xs-2 .MuiGrid-root")));
-                        }
-                    }
-                }
             }
+            System.out.println("sub_menu_List " + sub_menu_List);
+
+            ArrayList<String> sub_menu_sort_List = new ArrayList<>();
+            for (int j = 0; j < sub_menu_List.size(); j++) {
+                sub_menu_sort_List.add(j, sub_menu_List.get(j));
+            }
+
+            Collections.sort(sub_menu_sort_List);
+            System.out.println("sub_menu_sort_List " + sub_menu_sort_List);
+            Assert.assertEquals(sub_menu_List, sub_menu_sort_List);
+            sub_menu_List.clear();
+            sub_menu_sort_List.clear();
 
             if (isElementPresent(driver, By.cssSelector("[data-testid=ExpandMoreIcon]"))) {
                 driver.findElement(By.cssSelector("[data-testid=ExpandMoreIcon]")).click();
                 wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".MuiCollapse-wrapperInner .treeItem-level-1 .treeItem-node h6")));
-                //TimeUnit.MILLISECONDS.sleep(500);
-
             }
+
 
         }
 
